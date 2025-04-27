cd4c8d872d7c68a42e5092001c4c2b4cf91fb80e
Matija Kočevar <matija.kocevar@reduxi.eu>
2025-04-27 06:18:57 +0200

    Update CORS

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
     poweredByHeader: false,
     async headers() {
@@ -7,7 +9,10 @@ const nextConfig: NextConfig = {
             {
                 source: "/api/:path*",
                 headers: [
-                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { 
+                        key: "Access-Control-Allow-Origin", 
+                        value: isDevelopment ? "https://localhost:5173" : "https://dev-portal-management.vercel.app" 
+                    },
                     { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
                     { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
                     { key: "Access-Control-Allow-Credentials", value: "true" },
