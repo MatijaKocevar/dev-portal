cfef72c1eb494d4fd5ec24fb042de7fdab3e4d8b
Matija Kočevar <matija.kocevar@reduxi.eu>
2025-05-09 14:06:43 +0200

    Update open api libs

--- app/api/assets/units/[id]/route.ts
+++ app/api/assets/units/[id]/route.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextResponse } from "next/server";
 import { unitsData, Unit } from "../_data";
+import { withAuth } from "../../../../../lib/auth";
 
 type UnitParams = {
     id: string;
@@ -9,19 +10,16 @@ type UnitParams = {
 /**
  * Get unit by id.
  * @auth: bearer
- * @params: UnitParams
+ * @pathParams: UnitParams
  * @response: Unit[]
  */
-export async function GET(
-    request: Request,
-    { params }: Readonly<{ params: Promise<{ id: string }> }>
-) {
-    const { id } = await params;
-    const unit = unitsData().find((unit) => unit.id === id);
+export const GET = withAuth(async (req: Request) => {
+    const id = req.url.split("/units/")[1];
+    const unit = unitsData().find((unit: Unit) => unit.id === id);
 
     if (!unit) {
         return NextResponse.json({ error: "Unit not found" }, { status: 404 });
     }
 
     return NextResponse.json(unit);
-}
+});
